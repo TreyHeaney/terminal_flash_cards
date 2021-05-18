4c2033b6fd3a23ab727c89b78f531aedf47aede2
Trey Heaney <anonib991@gmail.com>
2021-05-18 09:58:03 -0400

    Added static storage for scores

--- flash_cards/cards/storage.py
+++ flash_cards/cards/storage.py
@@ -22,8 +22,11 @@ def load_cards(cards):
     parsed = []
     for card in cards:
         parsed.append(Card(question=card, 
-                           answer=cards[card][0],
-                           dummy_answers=cards[card][1:]))
+                           answer=cards[card]['answers'][0],
+                           dummy_answers=cards[card]['answers'][1:],
+                           score=cards[card]['meta']['score'],
+                           wrong_streak=cards[card]['meta']['wrong_streak'],
+                           last_correct=cards[card]['meta']['last_correct']))
 
     return parsed
 
@@ -34,7 +37,13 @@ def save(groups):
     for group in groups:
         dictionary[group.name] = {}
         for card in group.cards:
-            dictionary[group.name][card.question] = [card.answer] + card.dummy_answers
+            dictionary[group.name][card.question] = {}
+            dictionary[group.name][card.question]['meta'] = {}
+            
+            dictionary[group.name][card.question]['answers'] = [card.answer] + card.dummy_answers
+            dictionary[group.name][card.question]['meta']['score'] = card.score
+            dictionary[group.name][card.question]['meta']['wrong_streak'] = card.wrong_streak
+            dictionary[group.name][card.question]['meta']['last_correct'] = card.last_correct
 
     file = open('./static/save.json', 'w')
     dump(dictionary, file)
